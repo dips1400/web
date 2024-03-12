7187a3cdc696a60afac4dbaf0299c6c92c480812
Dipti Gawde <dkgawde14@gmail.com>
2024-03-12 15:22:23 +0530

    Adding file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Image1 from "../src/images/emile-perron-xrVDYZRGdw4-unsplash.png";
+import Image2 from "../src/images/fotis-fotopoulos-6sAl6aQ4OWI-unsplash.png";
+import Image3 from "../src/images/james-harrison-vpOeXr5wmR4-unsplash.png";
+import Infocontainer from "./components/Infocontainer";
+import Container from "./components/Container";
+import Lowerfooter from "./components/Lowerfooter";
+
+
+const images = [Image1, Image2, Image3];
 
 function App() {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage(
+        currentImage === images.length - 1 ? 0 : currentImage + 1
+      );
+    }, 2500);
+
+    return () => clearInterval(interval);
+  }, [currentImage]);
+
+  const goToPrev = () => {
+    setCurrentImage(currentImage === 0 ? images.length - 1 : currentImage - 1);
+  };
+
+  const goToNext = () => {
+    setCurrentImage(currentImage === images.length - 1 ? 0 : currentImage + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <Navbar />
+      <div className="hero-section">
+        <div
+          className="carousel"
+          style={{ backgroundImage: `url(${images[currentImage]})` }}
         >
-          Learn React
-        </a>
-      </header>
+          <button className="arrow prev" onClick={goToPrev}>
+            &#10094;
+          </button>
+          <button className="arrow next" onClick={goToNext}>
+            &#10095;
+          </button>
+          <Infocontainer />
+        </div>
+      </div>
+      <Container />
+      <Lowerfooter />
     </div>
   );
 }
